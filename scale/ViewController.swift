638783e2a7cd8adaa79984ab229562357b96f16e
Johannes Boß <johannes.boss@jambit.com>
2016-02-06 19:09:43 +0100

    Initial commit

--- scale/ViewController.swift
+++ scale/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var grams: UILabel!
+
+
+    var currentForce: CGFloat! = 0
+    var tara: CGFloat = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +24,34 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first as UITouch?
+
+        currentForce = touch!.force
+        grams.text = forceToGrams(currentForce)
+    }
+
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first as UITouch?
+
+        currentForce = touch!.force
+        grams.text = forceToGrams(currentForce)
+    }
+
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        currentForce = 0
+        grams.text = forceToGrams(currentForce)
+    }
+
+    @IBAction func taraButton(sender: AnyObject) {
+        tara = currentForce
+        grams.text = forceToGrams(currentForce)
+    }
+
+    func forceToGrams(force:CGFloat) -> String{
+        return String(format: "%.2f g", (force-tara)/CGFloat(0.02))
     }
 
 
